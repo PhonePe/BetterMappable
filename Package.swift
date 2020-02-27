8a1fb3df155811b2528a0e4c9554fffb5624aaba
Srikanth KV <srikanth.kv@phonepe.com>
2020-02-27 15:03:10 +0530

    Added swift package manager support

---
+++ Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "BetterMappable",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+         .Package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", majorVersion: 3, minor: 4),
+    ],
+    swiftLanguageVersions: [.v5_1]
+)
