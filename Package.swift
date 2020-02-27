b8d875e5c39916d59d63533c93efe2dbba2764ed
Srikanth KV <srikanth.kv@phonepe.com>
2020-02-27 15:33:44 +0530

    Added swift package manager support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,42 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
     name: "BetterMappable",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(
+            name: "BetterMappable",
+            targets: [
+                "BetterMappable-iOS",
+                "BetterMappable-macOS",
+                "BetterMappable-tvOS",
+                "BetterMappable-watchOS",
+            ]
+        )
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "3.5.1")
+        .package(
+            url: "https://github.com/tristanhimmelman/ObjectMapper.git",
+            from: "3.5.1"
+        )
+    ],
+    targets: [
+        .target(name: "BetterMappable-iOS", dependencies: [], path: "Source"),
+        .target(name: "BetterMappable-macOS", dependencies: [], path: "Source"),
+        .target(name: "BetterMappable-tvOS", dependencies: [], path: "Source"),
+        .target(name: "BetterMappable-watchOS", dependencies: [], path: "Source"),
+        .testTarget(name: "BetterMappable-iOSTests", dependencies: ["BetterMappable"], path: "Tests"),
+        .testTarget(name: "BetterMappable-macOSTests", dependencies: ["BetterMappable"], path: "Tests"),
+        .testTarget(name: "BetterMappable-tvOSTests", dependencies: ["BetterMappable"], path: "Tests")
+    ],
+    swiftLanguageVersions: [
+        .version("5.1")
     ]
 )
